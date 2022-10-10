cd98c72e8b77ab5536ec18e3ef00bdea533df03b
Yeongil Yoon <askme14397@gmail.com>
2022-10-10 16:33:01 +0900

    Add personal-blog

---
+++ personal-blog/_next/static/EAUS-himNNy5yt37lBO-v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-0d50401bad8bf595.js","static/chunks/pages/index-8c8b0380105469c5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
